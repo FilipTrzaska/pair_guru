55a774a5a1d4047b514ee92aae42d5d7b034432f
Filip Trzaska <filiptrzaska.we@gmail.com>
2017-12-13 05:31:13 +0000

    bracket validation added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,23 @@
 #
 
 class Movie < ApplicationRecord
+  validates :title, if: brackets_balanced?()
   belongs_to :genre
   has_many :comments, dependent: :destroy
+  
+  def brackets_balanced?(string)
+    return false if string.length < 2
+    brackets_hash = {"(" => ")", "{" => "}", "[" => "]"}
+    brackets = []
+    string.each_char do |x|
+        if brackets_hash.keys.include?(x)
+            brackets.push(x, 'a')
+        elsif brackets_hash.values.include?(x) && brackets_hash[brackets.last] == x
+            brackets.pop
+        elsif !x.strip.empty? && brackets.last == 'a'
+            brackets.pop
+        end
+    end
+    return brackets.empty?
+  end
 end
